f8a4f5c550818507b40fc3337d726f0a2588eb0c
abdelrahmangasser555 <abdelrahmangasser2023@gmail.com>
2023-09-04 00:22:05 +0300

    masking

--- masking.py
+++ masking.py
@@ -12,11 +12,12 @@ cv.imshow('Blank Image', blank)
 
 #mask
 start = (blank.shape[1]//2, blank.shape[0]//2)
-mask = cv.rectangle(blank, (start[0]-100, start[1]-100), (start[0]+100, start[1]+100), 255, -1 )
-cv.imshow('Mask', mask)
+# mask = cv.rectangle(blank, (start[0]-100, start[1]-100), (start[0]+100, start[1]+100), 255, -1 )
+mask_text = cv.putText(blank, 'Mask', (start[0]-200, start[1]+20), cv.FONT_HERSHEY_TRIPLEX, 5.0, 255, 20)
+cv.imshow('Mask', mask_text)
 
 #masked image
-masked = cv.bitwise_and(img, img, mask=mask)
+masked = cv.bitwise_and(img, img, mask=mask_text)
 cv.imshow('Masked Image', masked)
 
 
