430303d7f2fc3074a93bb5d9ded33a4562561437
abdelrahmangasser555 <abdelrahmangasser2023@gmail.com>
2023-09-03 18:27:47 +0300

    masking step 1

---
+++ masking.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import numpy as np
+
+#masking the files
+#original image
+img = cv.imread('images/engineer group.jpeg')
+cv.imshow('enginnering group ', img)
+
+#blank image
+blank = np.zeros(img.shape[:2], dtype='uint8')
+cv.imshow('Blank Image', blank)
+
+#mask
+start = (blank.shape[1]//2, blank.shape[0]//2)
+mask = cv.rectangle(blank, (start[0]-100, start[1]-100), (start[0]+100, start[1]+100), 255, -1 )
+cv.imshow('Mask', mask)
+
+#masked image
+masked = cv.bitwise_and(img, img, mask=mask)
+cv.imshow('Masked Image', masked)
+
+
+#key waity and destroy all windows
+cv.waitKey(0)
+cv.destroyAllWindows()
